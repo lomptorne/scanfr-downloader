337e632b2b7ab76a95205d94bdebad1434c0fa29
Clément Zitouni <cazitouni@gmail.com>
2020-09-29 10:12:56 +0200

    add support for new link format

--- scanFR_Downloader.py
+++ scanFR_Downloader.py
@@ -9,12 +9,12 @@ from pathlib import Path, PureWindowsPath
 
 def main():
     
-    counter = 1
+    counter = 0
     path = os.getcwd()
     detrompino = 0 
     checker = False
     breaker = 0
-    
+    appendix = None
     raw_url = str(input("Enter first page image URL: "))
     separator = raw_url.count("/")
     base_url = raw_url.rsplit("/", 1)[0]
@@ -26,7 +26,10 @@ def main():
     raw_page = raw_url.rsplit("/", 1)[1]
     extension = "." + raw_page.split(".", 1)[1]
     numberino = raw_page.split(".", 1)[0]
-    
+
+    if len(raw_page) > 7:
+        appendix = raw_page.split("-", 1)[0]
+        
     if "00" in str(numberino) : 
         detrompino = "00"
     if detrompino != 0 : 
@@ -40,6 +43,8 @@ def main():
 
         full_url = base_url + "/" + detrompino + str(counter) + extension
         
+        if appendix != None :
+            full_url = base_url + "/" + appendix + "-" + detrompino + str(counter) + extension
         if counter >= 10 and checker == True :
             detrompino = "0"
             full_url = base_url + "/" + detrompino + str(counter) + extension
